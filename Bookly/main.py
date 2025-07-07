675fcd4dfa6998e23ac2d3558c8f8d96d33e82e6
RajChandan <raj.chandan91@gmail.com>
2025-07-08 00:50:58 +0530

    lifespan method

--- Bookly/main.py
+++ Bookly/main.py
@@ -1,18 +1,16 @@
-from fastapi import FastAPI,Header
+from fastapi import FastAPI, Header
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    print(f"Server is starting ...")
+    yield
+    print(f"server has stopped.")
+
 
 version = "v1"
-app = FastAPI(
-    title="bookly",
-    version=version
-)
+app = FastAPI(title="bookly", version=version, lifespan=life_span)
 
-app.include_router(book_router,prefix=f"/api/{version}/books")
-
-
-
-
-
-
-
-
+app.include_router(book_router, prefix=f"/api/{version}/books")
