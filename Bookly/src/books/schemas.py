dd9cf53e49cb4c656321fc55863ee8f6b9c31bd9
RajChandan <raj.chandan91@gmail.com>
2025-08-09 23:48:26 +0530

    service methods

--- Bookly/src/books/schemas.py
+++ Bookly/src/books/schemas.py
@@ -1,20 +1,27 @@
 from pydantic import BaseModel
+import uuid
+from datetime import datetime
+
+
+class Book(BaseModel):
+    uid: uuid.UUID
+    title: str
+    author: str
+    publisher: str
+    language: str
+    created_at: datetime
+    updated_at: datetime
 
 
 class BookCreateModel(BaseModel):
-    title : str
-    author : str
-
-class Book(BaseModel):
-    id:int
-    title:str
-    author:str
-    publisher:str
-    language:str
+    title: str
+    author: str
+    publisher: str
+    language: str
 
 
 class BookUpdateModel(BaseModel):
-    title:str
+    title: str
     author: str
     publisher: str
     language: str
--- Bookly/src/books/service.py
+++ Bookly/src/books/service.py
@@ -1,6 +1,30 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
+from .schemas import BookCreateModel, BookUpdateModel
+from sqlmodel import select, desc
+from .models import Book
 
 
 class BookService:
     async def get_all_books(self, session: AsyncSession):
+        statement = select(Book).order_by(desc(Book.created_at))
+        result = await session.exec(statement)
+        return result.all()
+
+    async def get_book(self, session: AsyncSession, book_uid: str):
+        statement = select(Book).where(Book.uid == book_uid)
+        result = await session.exec(statement)
+        book = result.one_or_none()
+        if not book:
+            raise ValueError(f"Book with uid {book_uid} not found")
+        return book
+
+    async def book_create(self, session: AsyncSession, book_data: BookCreateModel):
+        pass
+
+    async def book_update(
+        self, session: AsyncSession, book_uid: str, update_data: BookUpdateModel
+    ):
+        pass
+
+    async def book_delete(self, session: AsyncSession, book_uid: str):
         pass
