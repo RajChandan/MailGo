6c2f6b108beb97b52bf9e43126c2a45534f6df11
RajChandan <raj.chandan91@gmail.com>
2025-08-10 23:56:19 +0530

    service complete

--- Bookly/src/books/service.py
+++ Bookly/src/books/service.py
@@ -15,16 +15,36 @@ class BookService:
         result = await session.exec(statement)
         book = result.one_or_none()
         if not book:
-            raise ValueError(f"Book with uid {book_uid} not found")
+            return None
         return book
 
     async def book_create(self, session: AsyncSession, book_data: BookCreateModel):
-        pass
+        book_data_dict = book_data.model_dump()
+        new_book = Book(**book_data_dict)
+        session.add(new_book)
+
+        await session.commit()
+        return new_book
 
     async def book_update(
         self, session: AsyncSession, book_uid: str, update_data: BookUpdateModel
     ):
-        pass
+        book_to_update = self.get_book(session, book_uid)
+        if book_to_update:
+            book_to_update_dict = update_data.model_dump()
+            for k, v in book_to_update_dict.items():
+                setattr(book_to_update, k, v)
+
+            await session.commit()
+            return book_to_update
+        else:
+            return None
 
     async def book_delete(self, session: AsyncSession, book_uid: str):
-        pass
+        book_to_delete = self.get_book(session, book_uid)
+        if book_to_delete:
+            await session.delete(book_to_delete)
+            await session.commit()
+            return book_to_delete
+        else:
+            return None
