0bdae188ff98200a0cb9056226d87f542c2443ce
RajChandan <raj.chandan91@gmail.com>
2025-08-11 23:59:57 +0530

    session

--- Bookly/src/db/session.py
+++ Bookly/src/db/session.py
@@ -1,5 +1,7 @@
 from sqlmodel import create_engine, text, SQLModel
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio.session import AsyncSession
+from sqlalchemy.orm import sessionmaker
 from src.books.models import Book
 from src.config import Config
 
@@ -9,3 +11,11 @@ engine = AsyncEngine(create_engine(url=Config.DATABASE_URL, echo=True))
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(
+        bind=AsyncEngine, class_=AsyncSession, expire_on_commit=False
+    )
+    async with Session() as session:
+        yield session
